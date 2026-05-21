e6b7abfd3ba237fbb4fd277e0ea98877b419349d
m1m0zzz <contact.m1m0zzz@gmail.com>
2026-05-21 18:43:49 +0900

    chore(deps): @tailwindcss/vite -> @tailwindcss/postcss

--- builder/shared/astro.config.mjs
+++ builder/shared/astro.config.mjs
@@ -1,12 +1,8 @@
 import sitemap from "@astrojs/sitemap"
-import tailwindcss from "@tailwindcss/vite"
 import { defineConfig } from "astro/config"
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()],
-  },
   integrations: [sitemap()],
   // site: "TODO", // deployed URL
   trailingSlash: "never",
