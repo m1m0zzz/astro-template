08babd40a8c64518d0aa4d2ffe2150847cd73a91
m1m0zzz <contact.m1m0zzz@gmail.com>
2026-05-30 23:07:48 +0900

    update: prettierrc

--- blog/.prettierrc.ts
+++ blog/.prettierrc.ts
@@ -6,6 +6,7 @@ const config: Config = {
   semi: false,
   singleQuote: false,
   plugins: ["prettier-plugin-astro", "prettier-plugin-tailwindcss"],
+  tailwindStylesheet: "./src/styles/global.css",
   overrides: [
     {
       files: "*.astro",
